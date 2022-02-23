f4cdc8177df1fd062cb5a088bcd98fbe648b4a74
Nico Deblauwe <nico@bluepundit.eu>
2022-02-23 12:39:06 +0100

    layout fix

--- resources/views/components/formelements/_errorandcomment.blade.php
+++ resources/views/components/formelements/_errorandcomment.blade.php
@@ -1,7 +1,7 @@
 @if( $errors->first($name) )
     <span class="text-red-400 ml-2 text-sm">
         <i class="fas fa-exclamation-triangle"></i>&nbsp;
-        {{ $errors->first($name) }}
+        {!! $errors->first($name) !!}
     </span>
 @elseif( $comment ?? null)
     <span class="text-gray-400 ml-2 text-sm">
