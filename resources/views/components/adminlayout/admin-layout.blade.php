0cbf464ed692c6789ddfbb053d3042456ff3b806
Nico Deblauwe <nico@bluepundit.eu>
2022-05-09 10:13:26 +0200

    Update admin-layout.blade.php

--- resources/views/components/adminlayout/admin-layout.blade.php
+++ resources/views/components/adminlayout/admin-layout.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full bg-gray-100">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="min-h-full bg-gray-100 @if(config('app.env') !== 'production') border-l-4 border-red-500 @endif">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
