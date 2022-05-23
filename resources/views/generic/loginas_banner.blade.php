815d844ab796da355fd42fe867162219723190d9
Nico Deblauwe <nico@bluepundit.eu>
2022-05-23 20:24:05 +0200

    Update loginas_banner.blade.php

--- resources/views/generic/loginas_banner.blade.php
+++ resources/views/generic/loginas_banner.blade.php
@@ -1,8 +1,8 @@
 @if (Session::has('loginas') )
     <div class="bg-lime-100 text-center py-4 border-b border-lime-400 text-black">
         <i class="fa fa-exclamation-triangle text-lime-400"></i>&nbsp;
-        Opgelet, u handelt namens een andere gebruiker (<span class="font-bold">{{auth()->user()->name}}</span>).
-        Vergeet niet uit te loggen als u klaar bent!&nbsp;&nbsp;
-        <a class="p-1 bg-lime-400 text-white rounded" href="{{route('stoploginas', 1)}}"><i class="fas fa-sign-in-alt"></i>&nbsp;Terug naar admin</a>
+        Attention, you are acting on behalf of another user (<span class="font-bold">{{auth()->user()->name}}</span>).
+        Don't forget to logout when ready!&nbsp;&nbsp;
+        <a class="p-1 bg-lime-400 text-white rounded" href="{{route('stoploginas', 1)}}"><i class="fas fa-sign-in-alt"></i>&nbsp;Back to admin</a>
     </div>
 @endif
