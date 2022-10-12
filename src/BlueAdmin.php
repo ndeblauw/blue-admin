013042d64e154c640d2a75f428eb03e1cbb63f79
Nico Deblauwe <nico@bluepundit.eu>
2022-10-12 17:40:07 +0200

    remove unused code

--- src/BlueAdmin.php
+++ src/BlueAdmin.php
@@ -4,17 +4,5 @@ namespace Ndeblauw\BlueAdmin;
 
 class BlueAdmin
 {
-    public static function routes()
-    {
-        // Filepond routes
-        Route::prefix('filepond')->group(function () {
-            Route::post('/', [App\Http\Controllers\BlueAdmin\FilepondController::class, 'upload'])->name('filepond.upload');
-            Route::delete('/', [App\Http\Controllers\BlueAdmin\FilepondController::class, 'delete'])->name('filepond.delete');
-        });
 
-// Login as routes
-        Route::get('admin/login-as/stop', [\App\Http\Controllers\BlueAdmin\UserLoginAsController::class, 'stopLoginAs'])->name('stoploginas');
-        Route::get('admin/login-as/{user}', [\App\Http\Controllers\BlueAdmin\UserLoginAsController::class, 'loginAs'])/*->middleware('isadmin')*/->name('loginas');
-
-    }
 }
