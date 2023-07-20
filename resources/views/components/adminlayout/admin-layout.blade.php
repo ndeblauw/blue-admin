4eff62af068e8afe5a6a73e2504ba3eaf3d5099c
Nico Deblauwe <nico@bluepundit.eu>
2023-07-20 18:54:53 +0200

    Update admin-layout.blade.php

--- resources/views/components/adminlayout/admin-layout.blade.php
+++ resources/views/components/adminlayout/admin-layout.blade.php
@@ -32,8 +32,7 @@
         }
     </style>
 
-    {{-- For admin backend statistics --}}
-    @if( config('app.env' === 'production') && config('blue-admin.fathom_site_id', false) )
+    @if( App::environment('production') && config('blue-admin.fathom_site_id', false) )
         <script src="https://cdn.usefathom.com/script.js" data-site="{{config('blue-admin.fathom_site_id')}}" defer></script>
     @endif
     
