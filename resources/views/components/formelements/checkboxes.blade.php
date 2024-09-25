ce124bf318c7980f39d4769278da3e48feb29abd
Nico Deblauwe <nico@bluepundit.eu>
2024-09-25 16:23:11 +0200

    BUGFIX :bug: Fix inline displaying of options

--- resources/views/components/formelements/checkboxes.blade.php
+++ resources/views/components/formelements/checkboxes.blade.php
@@ -1,9 +1,9 @@
 <div class="space-y-6 sm:space-y-5">
     <div class="sm:grid sm:grid-cols-6 sm:gap-4 sm:items-start sm:pt-5">
         @include('BlueAdminFormelements::_label')
-        <div class="mt-1 sm:mt-0 sm:col-span-5">
+        <div class="mt-1 sm:mt-0 sm:col-span-5 @if($inline) flex flex-row gap-x-4 @endif">
             @foreach($options as $optionValue => $optionLabel)
-                <div class="leading-loose @if($inline) @endif">
+                <div class="leading-loose">
                     <label class="">
                         <input type="checkbox" class="focus:ring-indigo-500 h-4 w-4 text-blue-600 border-gray-300 rounded" name="{{$name}}[]" value="{{$optionValue}}" id="{{$name}}-{{$optionValue}}"
                                @if(in_array($optionValue, old($name,$values))) checked @endif >
