9ea260fb4603b0574678aeee339c1c64e451e9d6
Nico Deblauwe <nico@bluepundit.eu>
2026-07-01 09:49:48 +0200

    Add Blue Admin Guidelines for AI Code Assistants

---
+++ resources/views/boost/guidelines/core.blade.php
@@ -0,0 +1,8 @@
+{{-- Blue Admin Guidelines for AI Code Assistants --}}
+{{-- Source: https://github.com/ndeblauw/blue-admin --}}
+{{-- License: (c) bluePundit --}}
+
+## Blue Admin Backend
+
+- `ndeblauw/blue-admin` to build flexible backend admin panels for any laravel website.
+- Always activate the `blue-admin-backend` skill when building an admin backend for a website.
